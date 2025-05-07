8126ae4dd845a923fa5409c10466d8a6ce400fb8
Kurapati Saiteja Reddy <155044949+Saitejare@users.noreply.github.com>
2025-05-07 23:27:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def ask():
             break
 
     return jsonify({'response': response})
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
